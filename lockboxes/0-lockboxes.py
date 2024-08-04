5224cff1cd4b16ae29db24f6861200471109e81c
MariamCHARRADA <charradamariam@yahoo.com>
2024-08-04 14:03:08 +0100

    interview

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that determines if all the boxes can be opened """
+
+
+def canUnlockAll(boxes):
+    """ determines if all the boxes can be opened """
+    
+    in_box = boxes.split(", ")
