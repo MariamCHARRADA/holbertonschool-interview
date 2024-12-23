967764740d0f819ac53224b9e07ad6b4d120c658
MariamCHARRADA <charradamariam@yahoo.com>
2024-12-23 12:12:59 +0100

    hello

--- wild_cmp/0-wildcmp.C
+++ wild_cmp/0-wildcmp.C
@@ -6,7 +6,7 @@
  * @s2: The second string, which may contain the wildcard '*'.
  * Return: 1 if the strings match, 0 otherwise.
  */
-int wildcmp(char *s1, char *s2)
+int wildcmp(char *s1, char *s2) 
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
