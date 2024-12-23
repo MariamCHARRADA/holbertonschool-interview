3f49c9a00a19d9531b3c2635c66b66f65ac345a1
MariamCHARRADA <charradamariam@yahoo.com>
2024-12-23 14:47:46 +0100

    test

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -3,17 +3,10 @@
 
 
 def isWinner(x, nums):
-    """Returns the winner of the game"""
     if not nums or x < 1:
         return None
 
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if n < 2:
-            continue
-
+    def count_primes(n):
         primes = [True] * (n + 1)
         primes[0] = primes[1] = False
 
@@ -22,8 +15,18 @@ def isWinner(x, nums):
                 for multiple in range(i * i, n + 1, i):
                     primes[multiple] = False
 
-        prime_count = sum(primes)
-        if prime_count % 2 == 1:
+        return sum(primes)
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if n < 2:
+            continue
+
+        primes = count_primes(n)
+
+        if primes % 2 == 1:
             maria_wins += 1
         else:
             ben_wins += 1
