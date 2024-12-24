ca97d91ac4f8cb823bce9a87bf110525fed97253
MariamCHARRADA <charradamariam@yahoo.com>
2024-12-24 17:27:16 +0100

    TTF

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,6 +3,21 @@
 
 
 def canUnlockAll(boxes):
-    """ determines if all the boxes can be opened """
-    
-    in_box = boxes.split(", ")
+    """determines if all the boxes can be opened"""
+
+    if not isinstance(boxes, list) or len(boxes) == 0:
+        return False
+
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
+    keys = [0]
+
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if 0 <= key < n and not opened[key]:
+                opened[key] = True
+                keys.append(key)
+
+    return all(opened)
