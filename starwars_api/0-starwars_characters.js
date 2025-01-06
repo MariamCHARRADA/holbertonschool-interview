5c0ecf1aa9a5840b86030bc452bb9b93d3b28e08
MariamCHARRADA <charradamariam@yahoo.com>
2025-01-06 23:17:25 +0100

    🚑 critical hotfix

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,29 +1,20 @@
 #!/usr/bin/node
-const request = require("request");
+const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-request(url, (error, response, body) => {
-  if (error || response.statusCode !== 200) {
-    console.error("Error:", error || `Status code: ${response.statusCode}`);
-    return;
-  }
-
-  const characters = JSON.parse(body).characters;
-
-  const fetchCharacter = (url) =>
-    new Promise((resolve) => {
-      request(url, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          console.log(JSON.parse(body).name);
-        }
-        resolve();
+request(url, async (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const chars = JSON.parse(body).characters;
+    for (const charUrl of chars) {
+      await new Promise((resolve, reject) => {
+        request(charUrl, async (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+            console.log(JSON.parse(body).name);
+          }
+          resolve();
+        });
       });
-    });
-
-  (async () => {
-    for (const charUrl of characters) {
-      await fetchCharacter(charUrl);
     }
-  })();
+  }
 });
