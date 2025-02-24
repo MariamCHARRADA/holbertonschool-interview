501afcc724bed6d5bbc1632c9cb6d19d6295f8ea
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-24 11:24:39 +0100

    code fix

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,33 +1,20 @@
 #!/usr/bin/node
-
 const request = require("request");
-
 const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(url, async (error, response, body) => {
-  if (error || response.statusCode !== 200) {
-    console.error("Error fetching movie data:", error || response.statusCode);
-    return;
-  }
-
-  const characters = JSON.parse(body).characters;
-
-  for (const characterUrl of characters) {
-    await new Promise((resolve, reject) => {
-      request(characterUrl, (error, response, body) => {
-        if (error || response.statusCode !== 200) {
-          console.error(
-            "Error fetching character data:",
-            error || response.statusCode
-          );
+  if (!error && response.statusCode === 200) {
+    const chars = JSON.parse(body).characters;
+    for (const charUrl of chars) {
+      await new Promise((resolve, reject) => {
+        request(charUrl, async (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+            console.log(JSON.parse(body).name);
+          }
           resolve();
-          return;
-        }
-
-        console.log(JSON.parse(body).name);
-        resolve();
+        });
       });
-    });
+    }
   }
 });
