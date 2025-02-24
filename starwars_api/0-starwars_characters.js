164eff4ac18a7e925dbf313636689a6d69445178
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-24 11:14:54 +0100

    hot fix

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,36 +1,31 @@
 #!/usr/bin/node
+
 const request = require("request");
+
 const movieId = process.argv[2];
-
-if (!movieId || isNaN(movieId) || parseInt(movieId) <= 0) {
-  console.error("Error: Please provide a valid positive movie ID.");
-  process.exit(1);
-}
-
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(url, async (error, response, body) => {
-  if (error) {
-    console.error("Error fetching data:", error);
-    process.exit(1);
+  if (error || response.statusCode !== 200) {
+    console.error("Error fetching movie data:", error || response.statusCode);
+    return;
   }
 
-  if (response.statusCode !== 200) {
-    console.error(
-      `Error: Unable to fetch movie. Status code: ${response.statusCode}`
-    );
-    process.exit(1);
-  }
+  const characters = JSON.parse(body).characters;
 
-  const chars = JSON.parse(body).characters;
-  for (const charUrl of chars) {
-    await new Promise((resolve) => {
-      request(charUrl, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          console.log(JSON.parse(body).name);
-        } else {
-          console.error(`Error fetching character: ${charUrl}`);
+  for (const characterUrl of characters) {
+    await new Promise((resolve, reject) => {
+      request(characterUrl, (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          console.error(
+            "Error fetching character data:",
+            error || response.statusCode
+          );
+          resolve();
+          return;
         }
+
+        console.log(JSON.parse(body).name);
         resolve();
       });
     });
